995a442ab23c157e4b423bc186ad4ab18502bd0e
Bruno Futema <bruno.futema@outlook.com>
2022-10-15 20:34:41 -0300

    fix: :hammer: Fix

--- main.tf
+++ main.tf
@@ -25,3 +25,13 @@ provider "aws" {
     }
   }
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "bfutema-remote-state"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
