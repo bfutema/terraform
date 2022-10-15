13a49f2f2a04facd0867854b3b9db0a5ea0f00cb
Bruno Futema <bruno.futema@outlook.com>
2022-10-15 20:15:34 -0300

    fix: :hammer: Remove data

--- main.tf
+++ main.tf
@@ -25,12 +25,3 @@ provider "aws" {
     }
   }
 }
-
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "bfutema-remote-state"
-    key    = "aws-vpc/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
